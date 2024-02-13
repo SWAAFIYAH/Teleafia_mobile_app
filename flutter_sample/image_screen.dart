1befd4ce1418d150eb92ac325cb57c61af8d8429
KenKunde <kenkunde94@gmail.com>
2024-02-13 15:53:58 +0300

    Add water

--- flutter_sample/image_screen.dart
+++ flutter_sample/image_screen.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
 
+class Demo extends StatelessWidget {
+  const Demo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Layout Demo'),
+      ),
+    );
+  }
+}
+
